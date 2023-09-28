2243e809124ef9d6397885474648816851436ecf
swathi <swathibalasubramanyam@gmail.com>
2023-09-28 14:18:42 -0700

    finished creating models and seeding the DB

--- practice-for-ch-react-pokedex-rails-backend-long-practice-main/pokedex-rails-backend/app/models/item.rb
+++ practice-for-ch-react-pokedex-rails-backend-long-practice-main/pokedex-rails-backend/app/models/item.rb
@@ -1,2 +1,21 @@
+# == Schema Information
+#
+# Table name: items
+#
+#  id         :bigint           not null, primary key
+#  pokemon_id :bigint           not null
+#  name       :string           not null
+#  price      :integer          not null
+#  happiness  :integer          not null
+#  image_url  :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Item < ApplicationRecord
+
+    validates :name, length: {in: 3..255, message: "%{value} should be of len 3-255"}
+
+    validates :price, numericality: {greater_than: 0, message: "%{value} should be gretaer than 0" }
+
+    belongs_to :pokemon
 end
--- practice-for-ch-react-pokedex-rails-backend-long-practice-main/pokedex-rails-backend/app/models/pokemon.rb
+++ practice-for-ch-react-pokedex-rails-backend-long-practice-main/pokedex-rails-backend/app/models/pokemon.rb
@@ -1,2 +1,58 @@
+# == Schema Information
+#
+# Table name: pokemons
+#
+#  id         :bigint           not null, primary key
+#  number     :integer          not null
+#  name       :string           not null
+#  attack     :integer          not null
+#  defense    :integer          not null
+#  poke_type  :string           not null
+#  image_url  :string           not null
+#  captured   :boolean          default(FALSE), not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Pokemon < ApplicationRecord
+
+    TYPES = [
+    'fire',
+    'electric',
+    'normal',
+    'ghost',
+    'psychic',
+    'water',
+    'bug',
+    'dragon',
+    'grass',
+    'fighting',
+    'ice',
+    'flying',
+    'poison',
+    'ground',
+    'rock',
+    'steel'
+  ].sort.freeze
+
+    validates :image_url, presence: true
+
+    validates :captured, inclusion: [true, false]
+
+    validates :name, length: {in: 3..255}, uniqueness: {message: "%{value} has been taken"}, presence: true
+
+    validates :attack, :defense, numericality: {in: 0..100, message: "%{value} should be within 0-100" }
+
+    validates :number, numericality: {greater_than: 0, message: "%{value} should be gretaer than 0" }
+
+    validates :poke_type, inclusion: {in: TYPES, message: "%{value} is not a valid poke type"}
+
+    has_many :poke_moves,
+        dependent: :destroy
+
+    has_many :moves,
+        through: :poke_moves
+
+    has_many :items,
+        dependent: :destroy
+
 end
